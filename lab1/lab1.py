2e5de5740172e7cb8c8a5d323d7fc77a400c476e
TariniS <tareene@gmail.com>
2020-12-21 14:06:44 -0800

    Add files via upload

---
+++ lab1/lab1.py
@@ -0,0 +1,11 @@
+#
+# This is a header block example for lab 1.
+#
+# You will need to supply the following information.
+#
+# Name:Tarini Srikanth
+# Instructor:Professor Turner
+# Section:101-05
+#
+
+print ("Hello, Tarini.")
